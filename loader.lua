8323c92010b76aa2cfc604a4c06789e93fc15289
mightyfleaaa and Xpl0its_Bop2 <e21.jan.orvalencia@suizoamericano.edu.gt>
2024-11-18 00:26:57 -0600

    -

--- loader.lua
+++ loader.lua
@@ -1,7 +1,9 @@
 if game.PlaceId == 18984416148 then
---
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sitiosweb24/Dandy-s-World-Bang-Script-Gui/refs/heads/main/main.lua"))()
+  
 else
 
---
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sitiosweb24/Dandy-s-World-Bang-Script-Gui/refs/heads/main/main2.lua"))()
 
 end
