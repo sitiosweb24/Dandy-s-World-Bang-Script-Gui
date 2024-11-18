5e021295c2bb3d2bd2d7b5848eabf607f9d41d37
mightyfleaaa and Xpl0its_Bop2 <e21.jan.orvalencia@suizoamericano.edu.gt>
2024-11-18 00:24:36 -0600

    -

---
+++ main.lua
@@ -0,0 +1,15 @@
+if game.PlaceId == 18984416148 then
+--
+else
+
+	local Gui = game:GetService("StarterGui")
+
+	Gui:SetCore("SendNotification", {
+		Title = "ERROR!";
+		Text = "This script is only for official dandy's world RP.";
+		Duration = 5;
+		--Callback here
+		Button1 = "OK.";
+	})
+
+end
