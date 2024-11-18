bad99d0ef9ff92bffac817321abc44a5281da8e6
mightyfleaaa and Xpl0its_Bop2 <e21.jan.orvalencia@suizoamericano.edu.gt>
2024-11-18 00:25:59 -0600

    -

---
+++ main2.lua
@@ -0,0 +1,9 @@
+	local Gui = game:GetService("StarterGui")
+
+	Gui:SetCore("SendNotification", {
+		Title = "ERROR!";
+		Text = "This script is only for official dandy's world RP.";
+		Duration = 5;
+		--Callback here
+		Button1 = "OK.";
+	})
