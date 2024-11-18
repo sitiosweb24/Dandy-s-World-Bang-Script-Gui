20ea7b99172eb09e80f1720c2210b3a637a7367c
mightyfleaaa and Xpl0its_Bop2 <e21.jan.orvalencia@suizoamericano.edu.gt>
2024-11-18 14:33:04 -0600

    -

--- loader.lua
+++ loader.lua
@@ -1,6 +1,10 @@
 if game.PlaceId == 18984416148 then
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sitiosweb24/Dandy-s-World-Bang-Script-Gui/refs/heads/main/main.lua"))()
+
+elseif game.PlaceId == 16116270224 then
+
+--print("in-lobby")
   
 else
 
